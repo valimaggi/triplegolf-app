eb4dcb1a6ff57d56e1bcfd480e803430031ff397
valimaggi <valimaggi@gmail.com>
2017-03-26 11:42:05 +0300

    PageHeader capitalizes the given header text if string

--- app/components/PageHeader.jsx
+++ app/components/PageHeader.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export default ({header}) => {
-  return (
-    <div style={{fontSize: 50,margin: 10}}>
-      {header}
-    </div>
-  );
-}
+const PageHeader = ({ header }) => (
+  <div style={{ fontSize: 50, margin: 10 }}>
+    {header && header.replace(/\b\w/g, l => l.toUpperCase())}
+  </div>
+);
+
+PageHeader.propTypes = {
+  header: PropTypes.string,
+};
+
+export default PageHeader;
