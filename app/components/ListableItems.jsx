c1ca0fd51c4d93ca054cd0050b39693d70727d03
valimaggi <valimaggi@gmail.com>
2017-04-26 23:43:33 +0300

    Added general component ListableItems which lists things in ListGroup of react-bootstrap (so preferably use ListGroupItems in children)

---
+++ app/components/ListableItems.jsx
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import { Row, Col, ListGroup } from 'react-bootstrap';
+
+const ListableItems = ({ colWidthMd, children }) => (
+  <Row>
+    <Col md={colWidthMd}>
+      <ListGroup>
+        {children}
+      </ListGroup>
+    </Col>
+  </Row>
+);
+
+ListableItems.propTypes = {
+  colWidthMd: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+ListableItems.defaultProps = {
+  colWidthMd: 6
+};
+
+export default ListableItems;
