6b589eeb21ecfc51224e054e657813a032909b62
valimaggi <valimaggi@gmail.com>
2017-04-27 00:11:29 +0300

    Comments added for why splitting store and root to two files based on dev/prod env.

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,3 +1,4 @@
+// Splitting to two files suggested by Dan Abramov: https://github.com/gaearon/redux-devtools/blob/master/docs/Walkthrough.md
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./configureStore.prod');
 } else {
