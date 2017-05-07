9b6334f34b5c480e240c95a72fed9a52c0c55a5b
valimaggi <valimaggi@gmail.com>
2017-05-07 17:47:09 +0300

    Home view player box widths narrowed + red color to delete player button

--- app/views/home/Players.jsx
+++ app/views/home/Players.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
+import { Row, Col } from 'react-bootstrap';
 import Player from '../../components/Player';
 
 const Players = ({ players, ...props }) => (
-  <div>
-    {players.map(player => (
-      <Player key={player.get('id')} player={player} {...props} />
-    ))}
-  </div>
+  <Row>
+    <Col md={12}>
+      {players.map(player => (
+        <Player key={player.get('id')} player={player} {...props} />
+      ))}
+    </Col>
+  </Row>
 );
 
 Players.propTypes = {
--- app/views/sport/SportGroup.jsx
+++ app/views/sport/SportGroup.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { Map, List } from 'immutable';
-import { Panel, Row, Col, Button, Glyphicon } from 'react-bootstrap';
+import { Panel, Row, Col, ButtonToolbar, Button, Glyphicon } from 'react-bootstrap';
 import Player from '../../components/Player';
 
 class SportGroup extends React.Component {
@@ -19,28 +19,22 @@ class SportGroup extends React.Component {
     const groupTitle = `Group ${group.get('id')}`;
 
     return (
-      <Panel header={groupTitle}>
+      <Panel header={groupTitle} bsStyle="success">
         <Row>
           <Col md={6}>
             {groupPlayers.map(player =>
               <Player key={player.get('id')} player={player} />
             )}
           </Col>
-          <Col md={1}>
-            <Row>
-              <Col md={12}>
-                <Link to={'/group/' + group.get('id')} >
-                  <Button bsSize="large" bsStyle="success"><Glyphicon glyph="zoom-in" /></Button>
-                </Link>
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col md={12}>
-                <Button bsSize="large" bsStyle="danger" onClick={() => this.deleteGroup(group.get('id'))}><Glyphicon glyph="remove" /></Button>
-              </Col>
-            </Row>
-
+          <Col md={6}>
+            <ButtonToolbar>
+              <Link to={'/group/' + group.get('id')} >
+                <Button bsSize="large" bsStyle="success"><Glyphicon glyph="zoom-in" /></Button>
+              </Link>
+              <Button bsSize="small" bsStyle="danger" className="pull-right" onClick={() => this.deleteGroup(group.get('id'))}>
+                <Glyphicon glyph="remove" />
+              </Button>
+            </ButtonToolbar>
           </Col>
         </Row>
       </Panel>
