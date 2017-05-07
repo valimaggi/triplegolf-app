c0091ea2b849b814c9c2bc80b9dde7eab4cdc1a4
valimaggi <valimaggi@gmail.com>
2017-05-08 01:06:19 +0300

    Group view opens now. Current hole and player shots can be modified.

--- app/views/group/Group.jsx
+++ app/views/group/Group.jsx
@@ -1,54 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'immutable';
+import { Row, Col, Panel, Button, Glyphicon } from 'react-bootstrap';
+import PageHeader from '../../components/PageHeader';
+import Player from '../../components/Player';
 import holes from '../../constants/holes';
+import { decrementCurrentHole, incrementCurrentHole } from './stateChanges';
+import { capitalize } from '../../util/helpers';
 
 class Group extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { currentHole: 1 };
   }
 
-  changeHole(group, holeIndexIncrement, maxHoleIndex, e) {
-    e.stopPropagation();
-    const newHoleIndex = group.get('currentHoleIndex') + holeIndexIncrement;
-    if (newHoleIndex >= 0 && newHoleIndex <= maxHoleIndex) {
-
-    }
+  decrementCurrentHole() {
+    this.setState(decrementCurrentHole);
   }
+
+  incrementCurrentHole() {
+    this.setState(incrementCurrentHole);
+  }
+
+  updatePlayer(playerId, shots) {
+    this.props.updatePlayer({ id: playerId, shots: shots });
+  }
+
   render() {
     const { groups, players, updatePlayer, ...props } = this.props;
     const group = groups.find(groupElem => groupElem.get('id') === props.params.id);
+    const groupName = `Group ${group.get('id')}`;
     const groupPlayers = players.filter(player => group.get('playerIds').includes(player.get('id')));
-    const holeNumberParMap = holes[group.get('sport')];
-    const holeNumbers = Object.keys(holeNumberParMap);
-    const maxHoleIndex = holeNumbers.length - 1;
-    const holeIndex = group.get('currentHoleIndex');
+    const holePars = holes[group.get('sport')];
+    const holeParText = `Hole ${this.state.currentHole} - Par ${holePars[this.state.currentHole]}`;
 
     return (
-      <div>
-        <h2>Group</h2>
-        <h3>{group.get('sport')}</h3>
-        <h4>Hole {holeNumbers[holeIndex]} - Par {holeNumberParMap[holeIndex+1]}</h4>
-        <div>
-          <span onClick={() => this.changeHole(group, -1, maxHoleIndex)}>Prev</span> -
-          <span onClick={() => this.changeHole(group, 1, maxHoleIndex)}>Next</span>
-        </div>
-        <div>
+      <Row>
+        <Col md={8}>
+          <PageHeader header={groupName} />
+          <h3>{capitalize(group.get('sport'))}</h3>
+          <Panel header={holeParText} >
+            <Row>
+              <Col md={8}>
+                <Button onClick={() => this.decrementCurrentHole()}><Glyphicon glyph="minus" /></Button>
+                <Button onClick={() => this.incrementCurrentHole()}><Glyphicon glyph="plus" /></Button>
+              </Col>
+            </Row>
+          </Panel>
           {groupPlayers.map(player =>
-            <div key={player.get('id')}>
-              <div key={player.get('id')}>
-              </div>
-              <div>
-                <button onClick={() => props.updatePlayer({id: player.get('id'), shots: player.get('shots')-1})}>-</button>
-                <button onClick={() => props.updatePlayer({id: player.get('id'), shots: player.get('shots')+1})}>+</button>
-              </div>
-              <br />
-            </div>
+            <Row key={player.get('id')}>
+              <Col md={6}>
+                <Player player={player} />
+                <Button onClick={() => this.updatePlayer(player.get('id'), player.get('shots') - 1)}><Glyphicon glyph="minus" /></Button>
+                <Button onClick={() => this.updatePlayer(player.get('id'), player.get('shots') + 1)}><Glyphicon glyph="plus" /></Button>
+              </Col>
+            </Row>
           )}
-        </div>
-      </div>
+        </Col>
+      </Row>
     );
   }
 }
---
+++ app/views/group/stateChanges.js
@@ -0,0 +1,4 @@
+
+export const decrementCurrentHole = prevState => ({ currentHole: prevState.currentHole - 1 });
+
+export const incrementCurrentHole = prevState => ({ currentHole: prevState.currentHole + 1 });
